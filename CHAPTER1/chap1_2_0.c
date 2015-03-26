cd50da72630d4397a9e58dd8aaef60f693935b1f
Alvin <alvin_yeats@163.com>
2015-03-26 11:15:45 +0800

    add chap1_2 test file

---
+++ CHAPTER1/chap1_2_0.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct pid_data
+{
+	float SetData;
+	float ActualData;
+	float err;
+	float err_last;
+	float Kp,Ki,Kd;
+	float sum;
+	float integral;
+}pid;
+
+void PID_init()
+{
+	printf("PID_init begin\n");
+	pid.SetData = 0.0;
+	pid.ActualData = 0.0;
+	pid.err = 0.0;
+	pid.err_last = 0.0;
+	pid.sum = 0.0;
+	pid.integral = 0.0;
+	pid.Kp = 0.2;
+	pid.Ki = 0.015;
+	pid.Kd = 0.2;
+	printf("PID_init end\n");
+}
+
+float PID_realize(float desired)
+{
+	pid.SetData     =  desired;
+	pid.err 	    =  pid.SetData - pid.ActualData;
+	pid.integral   +=  pid.err;
+	pid.sum         =  pid.Kp*pid.err + pid.Ki*pid.integral + pid.Kd*(pid.err - pid.err_last);
+	pid.err_last    =  pid.err;
+	pid.ActualData  =  pid.sum*1.0;
+
+	return pid.ActualData;
+}
+
+int main()
+{
+	printf("System begin \n");
+	PID_init();
+	int count = 0;
+	while(count < 1000)
+	{
+		float real= PID_realize(200.0);
+		printf("%f\n",real);
+		count++;
+	}
+
+	return 0;
+}
