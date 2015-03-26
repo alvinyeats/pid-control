ef5d1b71ba1d41177a54a29c9480ce835e1c594a
Alvin <alvin_yeats@163.com>
2015-03-26 16:06:16 +0800

    add a real book file chap1_2.c

---
+++ CHAPTER1/chap1_2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define	 Kp		20.0
+#define	 Kd		0.50
+#define	 Max	10.0
+#define	 Min 	8.0
+struct pid_data
+{
+	float SetData;
+	float ActualData;
+	float err;
+	float err_last;
+	float sum;
+}pid;
+
+
+void PID_init()
+{
+	printf("PID_init begin\n");
+	pid.SetData = 0.0;				
+	pid.ActualData = 8.0;		
+	pid.err = 0.0;
+	pid.err_last = 0.0;
+	pid.sum = 0.0;
+	printf("PID_init end\n");
+}
+
+float PID_realize(float desired)
+{
+	if(desired > Max)
+		desired = Max;
+	else if(desired < Min)
+		desired = Min;
+
+	pid.SetData     =  desired;
+	pid.err 	    =  pid.SetData - pid.ActualData;
+	pid.sum         =  Kp*pid.err + Kd*(pid.err - pid.err_last);
+	pid.err_last    =  pid.err;
+	pid.ActualData  =  pid.sum*1.0;
+
+	return pid.ActualData;
+}
+
+int main()
+{
+	printf("System begin \n");
+	PID_init();
+	int count = 0;
+	while(count < 100)
+	{
+		float real= PID_realize(11.0);
+		printf("%f\n",real);
+		count++;
+	}
+
+	return 0;
+}
