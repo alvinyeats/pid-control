2ca92abe61342dcfd526a4bd06c40033d9a6df83
Alvin <alvin_yeats@163.com>
2015-03-27 14:59:00 +0800

    add file chap1_4.c

---
+++ CHAPTER1/chap1_4.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define	 Kp		0.50
+#define  Ki		0.001
+#define	 Kd		0.001
+//#define  ts		0.001  //sampling time
+#define	 Max	10.0
+#define	 Min 	8.0
+struct pid_data
+{
+	float SetData;
+	float ActualData;
+	float err;
+	float err_last;
+	float sum;
+	float integral;
+}pid;
+
+
+void PID_init()
+{
+	printf("PID_init begin\n");
+	
+	pid.SetData = 0.0;				
+	pid.ActualData = 0.0;		
+	pid.err = 0.0;
+	pid.err_last = 0.0;
+	pid.sum = 0.0;
+	pid.integral = 0.0;
+	
+	printf("PID_init end\n");
+}
+
+float PID_realize(float desired)
+{
+	if(desired > Max)
+		desired = Max;
+	else if(desired < Min)
+		desired = Min;
+
+	pid.SetData     =  desired;
+	pid.err 	    =  pid.SetData - pid.ActualData;
+	pid.integral   +=  pid.err;
+	pid.sum         =  Kp*pid.err + Ki*pid.integral + Kd*(pid.err - pid.err_last);
+	pid.err_last    =  pid.err;
+	pid.ActualData  =  pid.sum*1.0;
+
+	return pid.ActualData;
+}
+
+int main()
+{
+	printf("System begin \n");
+	PID_init();
+	int count = 0;
+	while(count < 1000)
+	{
+		float real= PID_realize(11.0);
+		printf("%f\n",real);
+		count++;
+	}
+
+	return 0;
+}
