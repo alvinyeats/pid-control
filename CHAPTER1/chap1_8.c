4c174c95b967c2deac9d5b0cfc929254eb3c1147
Alvin <alvin_yeats@163.com>
2015-04-02 14:01:36 +0800

    add chap1_8.c and test chap1_7.c

--- CHAPTER1/chap1_8.c
+++ CHAPTER1/chap1_8.c
@@ -1,18 +1,13 @@
 /************************************************************************************
-*	The incomplete differential PID control algorithm
-*	不完全微分PID控制算法
+*	The derivative ahead PID control algorithm
+*	微分先行PID控制算法
 *
-*		在PID控制中，微分信号的引入可改善系统的动态性能，但也容易引进高频干扰，
-*	在误差扰动突变时尤其显出微分项的不足。若在控制算法中加入低通滤波器，则可使
-*	系统性能得到改善。
-*		克服上述缺点的方法之一是在PID算法中加入一个一阶惯性环节（低通滤波器）
-*	G_t(s)=1/(1+T_f_s)
-*		当M=1时，采用具有不完全微分PID方法；
-*		当M=2时，采用普通PID方法；
+*		取M=1，采用微分先行PID控制方法；
+*		取M=2，采用普通PID方法。
 *
-*		引入不完全微分，能有效的克服普通PID的不足。尽管不完全微分PID控制算法比普
-*	通PID控制算法要复杂些，但由于其良好的控制特性，近年来越来越得到广泛的应用。
-*	
+*		对于给定值rin(k)频繁升降的场合，引入微分先行后，可以避免给定值升降时
+*	所引起的系统振荡，明显地改善了系统的动态特性。
+*
 *	input:just for Step Signal
 *	目前只针对阶跃信号输入情况
 ************************************************************************************/
@@ -21,13 +16,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define  ts 	20
-#define  Tf 	180
-#define  TD 	140
+#define  gama 	0.50
+#define  Td 	Kd/Kp
+#define  Ti 	0.50
+#define	 ts 	20
 
-#define	 Kc		0.035
-#define  Ki		0.265
-#define	 Kd		(float)(Kc*TD)/ts
+#define	 Kp		0.63
+#define  Ki		0.252
+#define	 Kd		1.06
 
 //basic data of pid control 
 struct pid_data
@@ -59,25 +55,32 @@ struct pid_data* pid_init(float SetPoint, float FeedBack, float err, float err_l
 //The Increment PID Control Algorithm
 float pid_calc(pid_t* pid)
 {
-	float alfa,ud_k;
-	float ud_1=0;
-	int M = 1;
-	
+	float c1,c2,c3;
+	float ud_k,ud_1 = 0;
+	c1 = gama*Td/(gama*Td + ts);
+	c2 = (Td + ts)/(gama*Td + ts);
+	c3 = Td/(gama*Td + ts); 
+
 	pid->err = pid->SetPoint - pid->FeedBack;
 	pid->integral += pid->err;
+	
+	float y_1 = 0;
+	int M = 2;
+
 	if(M == 1)
 	{
-		alfa = Tf/(ts+Tf);
-		ud_k = Kd*(1 - alfa)*(pid->err - pid->err_last) + alfa*ud_1;
-		ud_1 = ud_k;
-		pid->u_k = Kc*pid->err + ud_k + Ki*pid->integral;
+		ud_k = c1*ud_1 + c2*pid->FeedBack - c3*y_1;
+		pid->u_k = Kp*pid->err + ud_k + Ki*pid->integral;
 	}
 	else if(M == 2)
 	{
-		pid->u_k = Kc*pid->err + Kd*(pid->err - pid->err_last) + Ki*pid->integral;
+		pid->u_k = Kp*pid->err + Kd*(pid->err - pid->err_last)/ts + Ki*pid->integral;
 	}
 
 	pid->FeedBack = pid->u_k*1.0;
+	
+
+	y_1 = pid->FeedBack;
 	pid->err_last = pid->err;
 
 	return pid->FeedBack;
@@ -91,9 +94,9 @@ int main()
 	int count = 0;
 	float real = 0;
 
-	tset = pid_init(35,0,0,0,0);
+	tset = pid_init(80,0,0,0,0);
 
-	while(count < 100)
+	while(count < 1000)
 	{
 		real = pid_calc(tset);
 		printf("%f\n",real);
